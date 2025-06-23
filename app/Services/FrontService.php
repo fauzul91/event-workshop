0d5418112d15dbc77595e2a1374dcb68886138cf
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-06-23 14:05:02 +0700

    feat: add service implementation for booking and front

---
+++ app/Services/FrontService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\WorkshopRepositoryInterface;
+
+class FrontService
+{
+    protected $categoryRepository;
+    protected $workshopRepository;
+
+    public function __construct(WorkshopRepositoryInterface $workshopRepository, CategoryRepositoryInterface $categoryRepository){
+        $this->categoryRepository = $categoryRepository;
+        $this->workshopRepository = $workshopRepository;
+    }
+
+    public function getFrontPageData()
+    {
+        $categories = $this->categoryRepository->getAllCategories();
+        $newWorkshops = $this->workshopRepository->getAllNewWorkshop();
+
+        return compact('categories', 'newWorkshops');
+    }
+}
